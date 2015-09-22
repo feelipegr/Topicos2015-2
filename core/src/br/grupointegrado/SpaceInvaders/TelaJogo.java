58e84ea64611973151137258c70e3c54c33c0146
Felipe Guilherme Rodrigues <feelipegr@gmail.com>
2015-09-21 21:10:24 -0300

    Aula 21/09

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,7 +1,9 @@
 package br.grupointegrado.SpaceInvaders;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
@@ -14,6 +16,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -179,6 +182,7 @@ public class TelaJogo extends TelaBase {
 		} else {
 			if (musicaFundo.isPlaying())
 				musicaFundo.stop();
+			reiniciarJogo();
 		}
 
 
@@ -191,6 +195,23 @@ public class TelaJogo extends TelaBase {
 		// desenha o palco de informacoes
 		palcoInformacoes.act(delta);
 		palcoInformacoes.draw();
+	}
+
+	/**
+	 * Verifica se o usuario pressionou ENTER para reiniciar o jogo
+	 */
+
+	private void reiniciarJogo() {
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+			Preferences preferencias = Gdx.app.getPreferences("SpaceInvaders");
+			int pontuacaoMaxima = preferencias.getInteger("pontuacao_maxima", 0);
+			if (pontuacao > pontuacaoMaxima) {
+				preferencias.putInteger("pontuacao_maxima", pontuacao);
+				preferencias.flush();
+			}
+
+			game.setScreen(new TelaMenu(game));
+		}
 	}
 
 	private void atualizarExplosoes(float delta) {
@@ -284,6 +305,7 @@ public class TelaJogo extends TelaBase {
 				if (meteoro.getY() + meteoro.getHeight() < 0) {
 					meteoro.remove(); // remove do palco
 					meteoros1.removeValue(meteoro, true); //remove da lista
+					pontuacao = pontuacao - 10;
 				}
 			}
 
@@ -295,6 +317,7 @@ public class TelaJogo extends TelaBase {
 				if (meteoro.getY() + meteoro.getHeight() < 0) {
 					meteoro.remove(); // remove do palco
 					meteoros2.removeValue(meteoro, true); //remove da lista
+					pontuacao = pontuacao - 30;
 				}
 			}
 	}
@@ -377,17 +400,51 @@ public class TelaJogo extends TelaBase {
 		indoEsquerda = false;
 		atirando = false;
 
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || clicouEsquerda()) {
 			indoEsquerda = true;
 		}
 
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || clicouDireita()) {
 			indoDireita = true;
 		}
 
-		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE) || Gdx.app.getType() == Application.ApplicationType.Android) {
 			atirando = true;
 		}
+	}
+
+	private boolean clicouEsquerda() {
+		if (Gdx.input.isTouched()) {
+			Vector3 posicao = new Vector3();
+			// captura clique/toque na janela no Windows
+			posicao.x = Gdx.input.getX();
+			posicao.y = Gdx.input.getY();
+			// converter para uma coordenada do jogo
+			posicao = camera.unproject(posicao);
+			float meio = camera.viewportWidth / 2;
+
+			if (posicao.x > meio) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean clicouDireita() {
+		if (Gdx.input.isTouched()) {
+			Vector3 posicao = new Vector3();
+			// captura clique/toque na janela no Windows
+			posicao.x = Gdx.input.getX();
+			posicao.y = Gdx.input.getY();
+			// converter para uma coordenada do jogo
+			posicao = camera.unproject(posicao);
+			float meio = camera.viewportWidth / 2;
+
+			if (posicao.x < meio) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
